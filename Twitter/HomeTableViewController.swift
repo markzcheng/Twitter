138aa97474114abd8f524c61980dd7e5f9529399
Mark Cheng <markcheng@wireless-10-148-182-114.public.utexas.edu>
2019-02-24 17:52:13 -0600

    logout

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Mark Cheng on 2/24/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
